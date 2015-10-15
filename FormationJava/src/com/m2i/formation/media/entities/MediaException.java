74b018126630f064293f4d9f8cacc3ac898734e6
adminlocal <adminlocal@Z1-PC.formation.local>
2015-10-15 11:57:22 +0200

    commit Eentity new without warning on packages

--- FormationJava/src/com/m2i/formation/media/entities/MediaException.java
+++ FormationJava/src/com/m2i/formation/media/entities/MediaException.java
@@ -1,5 +1,6 @@
 package com.m2i.formation.media.entities;
 
+@SuppressWarnings("serial") // That is what i'll have to do after
 public class MediaException extends Exception {
 	public MediaException(String msg){
 		super(msg);
